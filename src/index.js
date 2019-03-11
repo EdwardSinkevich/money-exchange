5bd6e5d383ce1fb2a3130c32ec027c201a141d1f
Edward <mejik-yo@mail.ru>
2019-03-11 18:01:52 +0300

    feat: implement function, which determine the minimum number of coins

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+  let moneyObj = {};
+  if (currency === 0) {
+    return moneyObj;
+  }
+  if (currency >= 10000) {
+    moneyObj.error = "You are rich, my friend! We don't have so much coins for exchange";
+    return moneyObj;
+  }
+  if (parseInt(currency / 50) > 0) {
+    moneyObj.H = parseInt(currency / 50);
+    currency = currency % 50;
+  }
+  if (parseInt(currency / 25) > 0) {
+    moneyObj.Q = parseInt(currency / 25);
+    currency = currency % 25;
+  }
+  if (parseInt(currency / 10) > 0) {
+    moneyObj.D = parseInt(currency / 10);
+    currency = currency % 10;
+  }
+  if (parseInt(currency / 5) > 0) {
+    moneyObj.N = parseInt(currency / 5);
+    currency = currency % 5;
+  }
+  if (parseInt(currency / 1) > 0) {
+    moneyObj.P = parseInt(currency / 1);
+  }
+  return moneyObj;
 }
